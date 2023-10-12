8985cd6b6cb0846e8e63fb4c0d0c00600b50bd48
khusan2006 <bahodirovkhusan@gmail.com>
2023-10-12 21:20:40 +0500

    finished body part of the project

--- src/components/CalcBody.jsx
+++ src/components/CalcBody.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const CalcBody = () => {
   return (
-    <div>
-        Body
+    <div className='calc-body'>
+        <span className='body__history'>History</span>
+        <span className='body__voice'>voice</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span className='body__del'>DEL</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>+</span>
+        <span>3</span>
+        <span>2</span>
+        <span>1</span>
+        <span>-</span>
+        <span>.</span>
+        <span>0</span>
+        <span>/</span>
+        <span>x</span>
+        <span className='body__reset'>Reset</span>
+        <span className='body__equal'>=</span>
     </div>
   )
 }
