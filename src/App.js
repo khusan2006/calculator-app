34d108efd4d900e9623e8689237b23afe65bf7e3
khusan2006 <bahodirovkhusan@gmail.com>
2023-10-13 19:39:00 +0500

    added some features

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import CalcHeader from "./components/CalcHeader";
 import CalcScreen from "./components/CalcScreen";
 
 function App() {
-  const [currentTheme, setCurrentTheme] = useState('dark')
+  const [currentTheme, setCurrentTheme] = useState('dark');
+  const [inputNumber, setInputNumber] = useState('');
+  const [firstNumber, setFirstNumber] = useState('');
+  const [secondNumber, setSecondNumber] = useState('')
+
+  const handleIncrement = () => {
+    if(!firstNumber) {
+      setFirstNumber(inputNumber)
+      setInputNumber('')
+    }else{
+      setInputNumber(Number(firstNumber) + Number(inputNumber))
+    }
+  }
+
+
   useEffect(() => {
     let localSTheme = localStorage.getItem('theme');
    
@@ -13,16 +27,56 @@ function App() {
     }else{
       setCurrentTheme(localSTheme)
     }
-
-
     document.documentElement.setAttribute('data-theme', currentTheme);
   },[currentTheme])
+
+
+  // useEffect(() => {
+  //   document.addEventListener('keydown', event => {
+  //     const key = event.key;
+    
+  //     if (key === 'Escape') {
+  //         // resetCalculator();
+  //         return;
+  //     }
+  
+  //     if (key === 'Backspace') {
+  //         setInputNumber((prev) => [...prev].splice(0,prev.length-1).join())
+  //         return;
+  //     }
+  
+  //     const keyNumber = +key;
+  //     if (keyNumber >= 0 && keyNumber <= 9) {
+  //         // getElement(`[data-key="${keyNumber}"]`).click();
+  //         // getElement(`[data-key="${keyNumber}"]`).focus();
+  //         return;
+  //     }
+  
+  //     if (key === '.') {
+  //         // getElement(`[data-key="."]`).click();
+  //         // getElement(`[data-key="."]`).focus();
+  //         return;
+  //     }
+  
+  //     if (['+', '-', '/', '*'].includes(key)) {
+  //         // getElement(`[data-operator="${key}"]`).click();
+  //         // getElement(`[data-operator="${key}"]`).focus();
+  //         return;
+  //     }
+  
+  //     if (key === 'Enter') {
+  //         event.preventDefault();
+  //         // calculatorEqualButton.click();
+  //         // calculatorEqualButton.focus();
+  //     }
+  // })
+  // },[])
   return (
     <div className="app">
         <div className="container">
             <CalcHeader setCurrentTheme={setCurrentTheme} />
-            <CalcScreen />
-            <CalcBody />
+            <CalcScreen inputNumber={inputNumber} setInputNumber={setInputNumber} />
+            <CalcBody setInputNumber={setInputNumber} handleIncrement={handleIncrement}  inputNumber={inputNumber} />
         </div>
     </div>
   );
