c0a45042bd62a62b3941be49ba48d999cfd32440
khusan2006 <bahodirovkhusan@gmail.com>
2023-10-15 11:55:06 +0500

    fixed bug that is happening when user click keyboard

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ function App() {
   const [inputNumber, setInputNumber] = useState('');
   const [firstNumber, setFirstNumber] = useState('');
   const [operationType, setOperationType] = useState('')
-  // const InputNumberLength = inputNumber.length
   // handling changes
   const handleInputChange = (value) => {
     setInputNumber((prev)=> prev.length <= MAX_VALUE ? `${prev}${value}` : prev)
+  }
+  const handleDelete = () => {
+    setInputNumber((prev) => prev.slice(0,-1))
   }
   //handling operations
  const handleOperations = (type='') => {
@@ -51,6 +53,31 @@ function App() {
     setInputNumber('')
     setFirstNumber('')
   }
+
+  const handlekeyboards = (e) => {
+    const {key} = e
+    if(key === 'Backspace') {
+      handleDelete() 
+    }
+
+    const keyNumber = +key;
+
+    if(keyNumber >= 0 && keyNumber <= 9) {
+      handleInputChange(keyNumber)
+    }
+
+    if(key === '.') {
+      handleInputChange(key)
+    }
+    
+    if(['+','-','*','/'].includes(key)) {
+      handleOperations(key)
+    }
+    if(key === '=' || key === 'Enter') {
+      firstNumber && handleOperations()
+    }
+  }
+
   // effect for handling theme 
   useEffect(() => {
     let localSTheme = localStorage.getItem('theme');
@@ -62,44 +89,23 @@ function App() {
     document.documentElement.setAttribute('data-theme', currentTheme);
   },[currentTheme])
 
+  
   //effect for handling keyboard events 
   useEffect(() => {
-    const handlekeyboards = (e) => {
-      const {key} = e
-      if(key === 'Backspace') {
-        setInputNumber((prev) => prev.slice(0,-1))
-      }
 
-      const keyNumber = +key;
-
-      if(keyNumber >= 0 && keyNumber <= 9) {
-        handleInputChange(keyNumber)
-      }
-
-      if(key === '.') {
-        handleInputChange(key)
-      }
-      
-      if(['+','-','*','/'].includes(key)) {
-        handleOperations(key)
-      }
-      if(key === '=' || key === 'Enter') {
-        handleOperations()
-      }
-    }
     document.addEventListener('keydown', handlekeyboards)
 
     return function() {
       document.removeEventListener('keydown', handlekeyboards)
     }
-  },[])
+  },[inputNumber, firstNumber])
 
   return (
     <div className="app">
         <div className="container">
             <CalcHeader setCurrentTheme={setCurrentTheme} />
             <CalcScreen inputNumber={inputNumber} setInputNumber={setInputNumber} MAX_VALUE={MAX_VALUE} />
-            <CalcBody handleOperations={handleOperations} handleInputChange={handleInputChange} reset={reset} />
+            <CalcBody firstNumber={firstNumber} handleOperations={handleOperations} handleInputChange={handleInputChange} reset={reset} handleDelete={handleDelete} />
         </div>
     </div>
   );
